de076b1604f49d2c88e98e4451d79e1aaedfcf93
213 <107265321+DanikDev123@users.noreply.github.com>
2024-11-21 12:29:15 +0300

    Update ex5.py

--- ex5.py
+++ ex5.py
@@ -1,5 +1,4 @@
-
-a = int(input("Enter a four digit number: "))
-
-
-
+number = int(input("Enter a four digit number: "))
+reversedNumberStr= str(number)[::-1]
+reversedNumber = int(reversedNumberStr)
+print ("Reversed number is: ",reversedNumber)
